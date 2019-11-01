665196b89ba0a83fa9bfb69b1e3518c12d174d31
iHugTrees <igoralikin@gmail.com>
2019-11-01 16:56:59 +0200

    started alittle

--- Components/OrGate.cs
+++ Components/OrGate.cs
@@ -8,12 +8,21 @@ namespace Components
     //This gate implements the or operation. To implement it, follow the example in the And gate.
     class OrGate : TwoInputGate
     {
-        //your code here 
+        //we will use a * and a * after it
+        private NotGate m_gNot;
+        private NAndGate m_gNand;
 
         public OrGate()
         {
-            //your code here 
-
+            //init the gates
+            m_gNand = new NAndGate();
+            m_gNot = new NotGate();
+            //wire the output of the nand gate to the input of the not
+            m_gNot.ConnectInput(m_gNand.Output);
+            //set the inputs and the output of the and gate
+            Output = m_gNot.Output;
+            Input1 = m_gNand.Input1;
+            Input2 = m_gNand.Input2;
         }
 
 
@@ -24,7 +33,23 @@ namespace Components
 
         public override bool TestGate()
         {
-            throw new NotImplementedException();
+            Input1.Value = 0;
+            Input2.Value = 0;
+            if (Output.Value != 0)
+                return false;
+            Input1.Value = 0;
+            Input2.Value = 1;
+            if (Output.Value != 1)
+                return false;
+            Input1.Value = 1;
+            Input2.Value = 0;
+            if (Output.Value != 1)
+                return false;
+            Input1.Value = 1;
+            Input2.Value = 1;
+            if (Output.Value != 1)
+                return false;
+            return true;
         }
     }
 
--- Components/XorGate.cs
+++ Components/XorGate.cs
@@ -8,11 +8,20 @@ namespace Components
     //This gate implements the xor operation. To implement it, follow the example in the And gate.
     class XorGate : TwoInputGate
     {
-        //your code here
-
+        //we will use a * and a * after it
+        private NotGate m_gNot;
+        private NAndGate m_gNand;
         public XorGate()
         {
-            //your code here
+            //init the gates
+            m_gNand = new NAndGate();
+            m_gNot = new NotGate();
+            //wire the output of the nand gate to the input of the not
+            m_gNot.ConnectInput(m_gNand.Output);
+            //set the inputs and the output of the and gate
+            Output = m_gNot.Output;
+            Input1 = m_gNand.Input1;
+            Input2 = m_gNand.Input2;
         }
 
         //an implementation of the ToString method is called, e.g. when we use Console.WriteLine(xor)
@@ -27,7 +36,23 @@ namespace Components
         //we simply check whether the truth table is properly implemented.
         public override bool TestGate()
         {
-            throw new NotImplementedException();
+            Input1.Value = 0;
+            Input2.Value = 0;
+            if (Output.Value != 0)
+                return false;
+            Input1.Value = 0;
+            Input2.Value = 1;
+            if (Output.Value != 1)
+                return false;
+            Input1.Value = 1;
+            Input2.Value = 0;
+            if (Output.Value != 1)
+                return false;
+            Input1.Value = 1;
+            Input2.Value = 1;
+            if (Output.Value != 1)
+                return false;
+            return true;
         }
     }
 }
